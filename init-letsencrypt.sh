89fb26df4018f74002d2089a6ffb32945e2488df
Au Yeong Khai Weng <aykhaiweng@gmail.com>
2019-08-03 15:31:05 +0800

    Ease of setup over agnostic setup

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,12 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org *.example.org)
+source .env
+
+domains=$LETSENCRYPT_DOMAINS
 rsa_key_size=4096
 data_path="./volumes/certs"
-email="aykhaiweng@gmail.com" # Adding a valid address is strongly recommended
+email=$LETSENCRYPT_EMAIL # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
